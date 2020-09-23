4ad3e5c5957c11d6e2467b495a6fcdf0d250c951
Salih Bektaş <salihbektastr@gmail.com>
2020-09-24 01:57:51 +0300

    Add comments

--- CurrencyCalculator.py
+++ CurrencyCalculator.py
@@ -6,11 +6,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
 import json
 
-
+#Get lates currency rates
 url = "https://api.exchangeratesapi.io/latest?"
-
 result = requests.get(url)
-
 result = json.loads(result.text)
 
 rates = result["rates"]
@@ -25,12 +23,16 @@ class Calculator(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.calculateButton.clicked.connect(self.pressBtn)
+        #load currencies to combo boxes
         self.ui.comboBoxSource.addItems(currencies)
-        self.ui.comboBoxSource.setCurrentIndex(8)
-        self.ui.comboBoxSource.setStyleSheet("combobox-popup: 0;")
         self.ui.comboBoxTarget.addItems(currencies)
-        self.ui.comboBoxTarget.setCurrentIndex(31)
+        self.ui.comboBoxSource.setStyleSheet("combobox-popup: 0;")
         self.ui.comboBoxTarget.setStyleSheet("combobox-popup: 0;")
+        #select EUR on source combo box(currencies[8] is EUR)
+        self.ui.comboBoxSource.setCurrentIndex(8)
+        #select USD on target combo box(currencies[31] is USD)
+        self.ui.comboBoxTarget.setCurrentIndex(31)
+        #Calculates the USD value of 1 EUR with the two digits after the dot
         temp = rates["USD"] *100
         temp = int(temp)
         temp/= 100
@@ -41,32 +43,41 @@ class Calculator(QtWidgets.QMainWindow):
         target = self.ui.comboBoxTarget.currentText()
         source = self.ui.comboBoxSource.currentText()
         money = self.ui.source.text()
+
+        #extracts commas from user's input
         temp = str()
         for i in money:
             if i != ',':
                 temp = temp + i
             else:
                 temp = temp + '.'
+        
+        #calculate from the desired currency with the two digits after the dot
+        #if the result is less than 0.01, the calculation is made in the
+        #smaller unit than the two digits after the point.
         money = temp
         money = float(money)
         money /= float(rates[source])
         money *= float(rates[target])
         money *= 100
+
         i = 0
-        while money<1:
+        while money < 1:
             money *= 100
             i += 1
+
         money = int(money)
         money /= 100
+
         while i > 0:
             money /= 100
             i -= 1
+
+        #write result to target label    
         self.ui.target.setText(str(money))
 
 
-
-
-
+#Main
 calculator = QtWidgets.QApplication(sys.argv)
 win = Calculator()
 win.show()
